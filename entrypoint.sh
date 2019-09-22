7293a2a2ddb98dbc3faefd6dc42a370057977074
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2019-09-22 18:08:39 +0100

    defaulting dest dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Build parameters:"
+echo "Destination directory: ${INPUT_DESTINATION_DIR}"
+
 bundle install
-bundle exec jekyll build --verbose --destination "${INPUT_DESTINATION_DIR}"
-touch "${INPUT_DESTINATION_DIR}/.nojekyll"
+bundle exec jekyll build --verbose --destination "${INPUT_DESTINATION_DIR-_site}"
+touch "${INPUT_DESTINATION_DIR-site}/.nojekyll"
