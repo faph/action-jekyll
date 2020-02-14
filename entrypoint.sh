7e626ea071edfb94dca669b7ef1304fc10acc751
Scott Walters <me@scowalt.com>
2020-02-14 05:44:02 -0800

    Need to install jekyll bundle from source directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,9 @@ echo "INPUT_DESTINATION_DIR: ${INPUT_DESTINATION_DIR:=_site}"
 echo "INPUT_SOURCE_DIR: ${INPUT_SOURCE_DIR:=./}"
 echo "INPUT_ADD_NOJEKYLL_TAG: ${ADD_NOJEKYLL_TAG:=true}"
 
+pushd ${INPUT_SOURCE_DIR}
 bundle install
+popd
 bundle exec jekyll build --verbose --destination "${INPUT_DESTINATION_DIR}" --source "${INPUT_SOURCE_DIR}"
 
 if [ $ADD_NOJEKYLL_TAG = true ]
