9501b06a354d4ae696a8893fadcd6a8e55d622a0
Scott Walters <me@scowalt.com>
2020-02-14 06:11:48 -0800

    Fixed execution directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,13 @@ echo "INPUT_DESTINATION_DIR: ${INPUT_DESTINATION_DIR:=_site}"
 echo "INPUT_SOURCE_DIR: ${INPUT_SOURCE_DIR:=./}"
 echo "INPUT_ADD_NOJEKYLL_TAG: ${ADD_NOJEKYLL_TAG:=true}"
 
-pushd ${INPUT_SOURCE_DIR}
+DESTINATION="`pwd`/${INPUT_DESTINATION_DIR}"
+cd INPUT_SOURCE_DIR
 bundle install
-popd
-bundle exec jekyll build --verbose --destination "${INPUT_DESTINATION_DIR}" --source "${INPUT_SOURCE_DIR}"
+bundle exec jekyll build --verbose --destination "${DESTINATION}"
 
 if [ $ADD_NOJEKYLL_TAG = true ]
 then
 echo "Writing ${INPUT_DESTINATION_DIR}/.nojekyll"
-touch "${INPUT_DESTINATION_DIR}/.nojekyll"
+touch "${DESTINATION}/.nojekyll"
 fi
